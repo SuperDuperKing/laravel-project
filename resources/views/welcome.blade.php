de79a47054e02affb4e97714f864bfef82190d73
James Vince <funnyshenanigans@gmail.com>
2020-05-08 18:52:17 -0400

    First update

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+    <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Google</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+        
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .button {
+             background-color: #eceff4;
+              border: none;
+              color: black;
+              padding: 10px 25px;
+              text-align: center;
+              display: inline-block;
+              font-size: 16px;
+            }
+
+            .top-menu {
+                display: flex;
+
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+        <div class="top-menu">
+                      <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=webhp&cd=&cad=rja&uact=8&ved=0ahUKEwjbhqfijKXpAhXOhOAKHX-IBx8QkNQCCAM&url=https%3A%2F%2Fabout.google%2F%3Ffg%3D1%26utm_source%3Dgoogle-US%26utm_medium%3Dreferral%26utm_campaign%3Dhp-header&usg=AOvVaw13L0SpN0L7ycx9R0_i4R2S">About</a>
+                      <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=webhp&cd=&cad=rja&uact=8&ved=0ahUKEwjbhqfijKXpAhXOhOAKHX-IBx8QpMwCCAQ&url=https%3A%2F%2Fstore.google.com%2FUS%3Futm_source%3Dhp_header%26utm_medium%3Dgoogle_ooo%26utm_campaign%3DGS100042%26hl%3Den-US&usg=AOvVaw0wvkRBURf91YdMQCXOelDf">Store</a>
+                  </div>
+            <div class="content">
+                    <a href="#">
+                        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAAAoklEQVR42mNgmGnMQAZ2AOKZQPwAimdCxRBqyDT0DBD/R8NnUAwnw+CZWAyF4ZmUGPwAj8EPBp3BzkB8mdSgwBfTMkC8EsmAW8RGHq6YPgu14DOUD6IrgNiF2OSGL6ZheDUQyxEdbEREyCcgdiU5kkmKaTIMxhcUlylxMa7Iu0VpGONKbqDYL6ckVRDCZKdjmua84VkI0bQ8plkNQrM6jygMAPbbJ6MGdtX+AAAAAElFTkSuQmCC" alt="">
+                    </a>
+                  <img src="https://www.google.com/logos/doodles/2020/stay-and-play-at-home-with-popular-past-google-doodles-pac-man-2010-6753651837108775.2-law.gif" alt="">
+                  <div class="search-area">
+                        <div class=search-input>
+                            <button type=submit>
+                            <i class="fa fa-search fa-lg"></i>
+                            </button>
+                            <input type="text" name="Search" id="">
+                        </div>    
+                  </div>
+                <a href="#" class="button">Search</a>
+                <a href="#" class="button">I'm Feeling Lucky</a>
+                <br>
+                <br>
+                <a href="https://www.google.com/url?q=https://www.youtube.com/stayhome%3Futm_source%3Dgoogle%26utm_medium%3Dhppromo%26utm_campaign%3DHelpathomeYTUS&source=hpp&id=19017550&ct=3&usg=AFQjCNE2FZizHR5ncV3c9xnzo6f1UpGKmQ&sa=X&ved=0ahUKEwiD0e2SpaXpAhXFSt8KHWDnBSEQ8IcBCBE">Make the most of your time at home with tips for recipes, workouts, and more</a>
+            </div>
+        </div>
+    </body>
+</html>
